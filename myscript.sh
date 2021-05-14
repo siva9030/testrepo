8f51e81dcd8f53a5b8dba477415919080d6fe2e1
Siva Kumar <siva.09.avula@gmail.com>
2021-05-14 08:52:47 +0530

    test commit pust to github

--- myscript.sh
+++ myscript.sh
@@ -4,3 +4,16 @@ cd /c/WEZVA/Git/6sRepo
 echo "Hello New Apple 6s, welcome to the new covid world" > scr.txt
 
 touch siva.txt kasthu.txt
+
+dfskdjhskdj
+
+sdfasdkfjas
+asdfaksdfa
+
+
+adsfasdfas
+sdfafasdkfaskdhfkasdflkasfahsdfas
+
+
+dudbaadfjjjkfam
+
